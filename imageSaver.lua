e71bfc94bf22b6727c3046512b025ee810b53d82
ZENG Xiaohui <xzengaf@ust.hk>
2016-06-24 13:19:46 +0800

    Delete imageSaver.lua

--- imageSaver.lua
+++
@@ -1,8 +0,0 @@
--- imageSaver.lua
---[[
-to make life easier
-this function can save table or tensor with dimension more than 3
-specially, 
-- it can save weight tensor with multidepth as one image
-- it can save flow image in [H, W, D=2] with colorwheel indicated the direction of the flow and the magitude indicated by the lightness of the image
-]]
